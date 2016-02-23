b724ade72d5c752431d2ab8993c7b3a13d0ed874
Alan Pearson <pearsonalan@mac.com>
2016-02-22 22:51:14 -0800

    implement memcopy function; don't need to shift the address of data segment items

--- memcopy/memcopy.asm
+++ memcopy/memcopy.asm
@@ -49,9 +49,18 @@ main:
 	call	toggle_b0		; toggle B0 to HIGH, then LOW to store initial memory to dump
 
 	ldi	r25, BUFSZ		; load the size of the source buffer into r25
-	ldi	ZL, LOW(src<<1)		; load the address of the source buffer into ZH:ZL
-	ldi	ZH, HIGH(src<<1)
+	ldi	zl, LOW(src)		; load the address of the source buffer into zh:zl
+	ldi	zh, HIGH(src)
 	call	fill_buffer		; invoke the fill_buffer routine
+
+	call	toggle_b0		; toggle B0 to HIGH, then LOW to store memory to dump after fill_buffer
+
+	ldi	r25, BUFSZ		; load the size of the source buffer into r25
+	ldi	yl, LOW(src)		; load the address of the source buffer into yh:yl
+	ldi	yh, HIGH(src)
+	ldi	zl, LOW(dest)		; load the address of the destination buffer into zh:zl
+	ldi	zh, HIGH(dest)
+	call	memcopy			; invoke the memcopy routine
 
 	call	toggle_b0		; toggle B0 to HIGH, then LOW to store memory to dump after fill_buffer
 
@@ -86,11 +95,20 @@ toggle_b0:
 	ret
 
 
-;======================
+;================================
 ; fill_buffer subroutine
 ;
-; fill the buffer pointed to by Z (r31:r30) with bytes
-; from 0 .. r25
+; synopsis:
+;	Fill the buffer pointed to by Z (r31:r30) with bytes from 0 .. r25
+;
+; inputs:
+;	Z (R31:R30)	- pointer to buffer
+;	R25		- count of bytes to fill
+;
+; Registers altered:
+;	temp (R16)
+;	Z (R31:R30)
+;	R25
 
 fill_buffer:
 	clr	temp			; clear temp register
@@ -105,8 +123,40 @@ _fb_end:
 	ret
 
 
+;================================
+; memcopy subroutine
+;
+; synopsis:
+;	Copy up to 255 bytes from the buffer reference by the Y register pair
+;	to the buffer pointed to by Z register pair. The count of bytes to
+;	copy is passed in r25.
+;
+;	The source and destination buffers should not be overlapping
+;
+; inputs:
+;	Z (R31:R30)	- pointer to buffer to fill
+;	Y (R29:R28)	- pointer to source buffer
+;	R25		- count of bytes to fill
+;
+; Registers altered:
+;	temp (R16)
+;	Z (R31:R30)
+;	Y (R29:R28)
+;	R25
 
-;======================
+memcopy:
+	tst	r25		; test if done
+	breq	_memcopy_end	; jump to end if done
+	ld	temp, y+	; read byte into temp from Y pointer and increment pointer
+	st	z+, temp	; write byte to address in Z pointer and increment pointer
+	dec	r25		; decrement byte count register
+	rjmp	memcopy		; loop back
+_memcopy_end:
+	ret
+
+
+
+;================================
 ; Program data
 ;
 
@@ -116,7 +166,7 @@ message:
 	.db	"Hello World", $0D, $0A, 0
 
 
-;======================
+;================================
 ; data segment
 ;
 
@@ -124,4 +174,7 @@ message:
 
 ;; buffer for memcopy source
 src:	.byte	BUFSZ
+
+;; buffer for memcopy destination
+dest:	.byte	BUFSZ
 	
