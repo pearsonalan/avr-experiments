7059d837d9b5fa0a1d337329c24bc5f755f64f54
Alan Pearson <pearsonalan@mac.com>
2016-02-22 23:02:35 -0800

    Implment pmemcopy routine, change use of Y and Z register pointers to be consistent

--- memcopy/memcopy.asm
+++ memcopy/memcopy.asm
@@ -49,23 +49,29 @@ main:
 	call	toggle_b0		; toggle B0 to HIGH, then LOW to store initial memory to dump
 
 	ldi	r25, BUFSZ		; load the size of the source buffer into r25
-	ldi	zl, LOW(src)		; load the address of the source buffer into zh:zl
-	ldi	zh, HIGH(src)
+	ldi	yl, LOW(src)		; load the address of the source buffer into yh:yl
+	ldi	yh, HIGH(src)
 	call	fill_buffer		; invoke the fill_buffer routine
 
 	call	toggle_b0		; toggle B0 to HIGH, then LOW to store memory to dump after fill_buffer
 
 	ldi	r25, BUFSZ		; load the size of the source buffer into r25
-	ldi	yl, LOW(src)		; load the address of the source buffer into yh:yl
-	ldi	yh, HIGH(src)
-	ldi	zl, LOW(dest)		; load the address of the destination buffer into zh:zl
-	ldi	zh, HIGH(dest)
+	ldi	yl, LOW(dest)		; load the address of the destination buffer into yh:yl
+	ldi	yh, HIGH(dest)
+	ldi	zl, LOW(src)		; load the address of the source buffer into zh:zl
+	ldi	zh, HIGH(src)
 	call	memcopy			; invoke the memcopy routine
 
 	call	toggle_b0		; toggle B0 to HIGH, then LOW to store memory to dump after fill_buffer
 
+	ldi	r25, 14			; load the size of the message into r25
+	ldi	yl, LOW(dest)		; load the address of the destination buffer into yh:yl
+	ldi	yh, HIGH(dest)
 	ldi	zl, LOW(message<<1)	; load address of message into zh:zl
 	ldi	zh, HIGH(message<<1)	
+	call	pmemcopy		; invoke the pmemcopy routine
+
+	call	toggle_b0		; toggle B0 to HIGH, then LOW to store memory to dump after fill_buffer
 
 end:
 	nop
@@ -99,15 +105,15 @@ toggle_b0:
 ; fill_buffer subroutine
 ;
 ; synopsis:
-;	Fill the buffer pointed to by Z (r31:r30) with bytes from 0 .. r25
+;	Fill the buffer pointed to by Y (r29:r28) with bytes from 0 .. r25
 ;
 ; inputs:
-;	Z (R31:R30)	- pointer to buffer
+;	Y (R29:R28)	- pointer to buffer
 ;	R25		- count of bytes to fill
 ;
 ; Registers altered:
 ;	temp (R16)
-;	Z (R31:R30)
+;	Y (R29:R28)
 ;	R25
 
 fill_buffer:
@@ -115,7 +121,7 @@ fill_buffer:
 _fb_loop:
 	tst	r25			; test if done filling buffer
 	breq	_fb_end			; jump to end if done
-	st	z+, temp		; copy temp register into address pointed to by Z and increment pointer
+	st	y+, temp		; copy temp register into address pointed to by Y and increment pointer
 	inc	temp			; increment temp register
 	dec	r25			; decrememt counter register
 	rjmp	_fb_loop		; loop back
@@ -127,15 +133,16 @@ _fb_end:
 ; memcopy subroutine
 ;
 ; synopsis:
-;	Copy up to 255 bytes from the buffer reference by the Y register pair
-;	to the buffer pointed to by Z register pair. The count of bytes to
+;	Copy up to 255 bytes from the buffer referenced by the Z register pair
+;	to the buffer pointed to by Y register pair. The count of bytes to
 ;	copy is passed in r25.
 ;
-;	The source and destination buffers should not be overlapping
+;	The source and destination buffers should not be overlapping.  The source
+;	and destination buffers are also assumed to be in the data segment.
 ;
 ; inputs:
-;	Z (R31:R30)	- pointer to buffer to fill
-;	Y (R29:R28)	- pointer to source buffer
+;	Y (R29:R28)	- pointer to destination buffer
+;	Z (R31:R30)	- pointer to source buffer in data memory
 ;	R25		- count of bytes to fill
 ;
 ; Registers altered:
@@ -147,11 +154,48 @@ _fb_end:
 memcopy:
 	tst	r25		; test if done
 	breq	_memcopy_end	; jump to end if done
-	ld	temp, y+	; read byte into temp from Y pointer and increment pointer
-	st	z+, temp	; write byte to address in Z pointer and increment pointer
+	ld	temp, z+	; read byte into temp from Z pointer and increment pointer
+	st	y+, temp	; write byte to address in Y pointer and increment pointer
 	dec	r25		; decrement byte count register
 	rjmp	memcopy		; loop back
 _memcopy_end:
+	ret
+
+
+;================================
+; pmemcopy subroutine
+;
+; synopsis:
+;	Copy up to 255 bytes from the buffer referenced by the Z register pair
+;	to the buffer pointed to by Y register pair. The count of bytes to
+;	copy is passed in r25.
+;
+;	The source buffer (Z) must reference an address in program memory.
+;	The destination buffer (Y) must be in the data segment.
+;
+;	The subroutine is the same as the memcopy subroutine except that the
+;	LPM instruction is used to read program memory instad of the LD
+;	instruction which can only read data memory.
+;
+; inputs:
+;	Y (R29:R28)	- pointer to destination buffer in data memory
+;	Z (R31:R30)	- pointer to source buffer in program memory
+;	R25		- count of bytes to copy
+;
+; Registers altered:
+;	temp (R16)
+;	Z (R31:R30)
+;	Y (R29:R28)
+;	R25
+
+pmemcopy:
+	tst	r25		; test if done
+	breq	_pmemcopy_end	; jump to end if done
+	lpm	temp, z+	; read byte into temp from Z pointer and increment pointer
+	st	y+, temp	; write byte to address in Y pointer and increment pointer
+	dec	r25		; decrement byte count register
+	rjmp	pmemcopy	; loop back
+_pmemcopy_end:
 	ret
 
 
