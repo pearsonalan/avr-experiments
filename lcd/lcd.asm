19b20ebcbde556c677a36a67283c4806b983c1a2
Alan Pearson <pearsonalan@mac.com>
2016-03-24 00:19:10 -0700

    More work on LCD driver

--- lcd/lcd.asm
+++ lcd/lcd.asm
@@ -6,20 +6,59 @@
 ; for now, it supports 2x20 displays.
 ;
 ; The Device should be connected to the LCD as follows:
-;	B0 => DATA0
-;	B1 => DATA1
-;	B2 => DATA2
-;	B3 => DATA3
+;	B0 => DB4
+;	B1 => DB5
+;	B2 => DB6
+;	B3 => DB7
 ;	B4 => RS
 ;	B5 => ENABLE
 ;	B6 => READ/WRITE
 ;
 ;*****************************************************************************
 
+;; Include the standard definitions for PORTS and REGISTERS
+.nolist
+.include "../asmlib/m328Pdef.asm"
+.list
+
+
 ;==============
 ; Declarations:
 
 .def temp      = r16
+
+
+;
+; Set up the Interrupt Vector
+;
+
+.org 0x0000
+	jmp reset		; PC = 0x0000	RESET
+
+
+
+;======================
+; initialization
+
+.org 0x0034
+reset:
+	clr	r1			; set the SREG to 0
+	out	SREG, r1
+	ldi	r28, LOW(RAMEND)	; init the stack pointer
+	ldi	r29, HIGH(RAMEND)
+	out	SPL, r28
+	out	SPH, r29
+	
+	
+;======================
+; Main body of program:
+
+main:
+	rcall	lcd_init
+
+	;; infinite loop at the end to keep the PC from falling off the end of the world
+end:	rjmp	end			; do nothing forever
+
 
 
 ;================================
@@ -30,7 +69,7 @@
 ;
 ;	The number of loops performed is passed in the register pair
 ;	R25:R24. The maximum number of loops which can be performed
-;	is 65536 (which is achieved by passing $ff:$ff). Each iteration
+;	is 65536 (which is achieved by passing $00:$00). Each iteration
 ;	of the loop takes 4 clock cycles. With a 16MHz clock, a clock
 ;	cycle is 62.5 ns, so one iteration of the loop is 250 ns. The
 ;	maximum delay achievable is (65536 * 250ns) = 16,384 us.
@@ -56,22 +95,40 @@ basic_delay_l:
 	brne	basic_delay_l
 	ret
 
+;
 ; some fixed delays
-delay_10ms:
+;
+
+; 10ms => 40,000 iterations (40,000 = $9C40)
+delay_10_ms:
 	ldi	r25, $9c
 	ldi	r24, $40
 	rjmp	basic_delay_l
 
-delay_40ms:
-	clr	r25
-	clr	r24
-	rcall	basic_delay_l
-	clr	r25
-	clr	r24
-	rcall	basic_delay_l
-	ldi	r25, $1c
-	ldi	r24, $40
+; 4.1ms => 16,400 iterations ($4010)
+delay_4_1_ms:
+	ldi	r25, $40
+	ldi	r24, $10
 	rjmp	basic_delay_l
+
+; 100us => 400 iterations ($0190)
+delay_100_us:
+	ldi	r25, $01
+	ldi	r24, $90
+	rjmp	basic_delay_l
+
+; 40ms => 160,000 iterations => 65536 + 65536 + 28928
+delay_40_ms:
+	clr	r25
+	clr	r24
+	rcall	basic_delay_l
+	clr	r25
+	clr	r24
+	rcall	basic_delay_l
+	ldi	r25, $71		; 28928 => $7100
+	ldi	r24, $00
+	rjmp	basic_delay_l
+
 
 ;================================
 ; lcd_init subroutine
@@ -88,7 +145,22 @@ lcd_init:
 	out	DDRB, temp
 
 	; delay 40 ms
-	rcall	delay_40ms
+	rcall	delay_40_ms
+
+	; write RW=>0, RS=>0, E=>0 DB[4..7]=>$02
+	ldi	temp, $02
+	out	PORTB, temp
+
+	; wait 4.1 ms
+	rcall	delay_4_1_ms
+
+	; write RW=>0, RS=>0, E=>0 DB[4..7]=>$02
+	ldi	temp, $02
+	out	PORTB, temp
+
+	; wait 100 us
+	rcall	delay_100_us
 
 	ret
+
 
