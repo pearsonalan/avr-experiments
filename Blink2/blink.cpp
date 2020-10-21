a71831429ca3eb77b8d37d9d8243ada3ff340497
Alan Pearson <pearsonalan@mac.com>
2020-10-20 18:37:40 -0700

    Add some serial I/O

--- Blink2/blink.cpp
+++ Blink2/blink.cpp
@@ -14,14 +14,19 @@ void setup() {
   // Configure both LED pins as output
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+
+  // Set up Serial output
+  Serial.begin(115200);
 }
 
 // The loop function runs repeatedly
 void loop() {
+  Serial.println("LED STATE 0");
   digitalWrite(LED1, HIGH);   // turn the LED 1 on
   digitalWrite(LED2, LOW);    // turn the LED 2 off
   delay(1000);
 
+  Serial.println("LED STATE 1");
   digitalWrite(LED1, LOW);    // turn the LED 1 off
   digitalWrite(LED2, HIGH);   // turn the LED 2 on
   delay(1000);
