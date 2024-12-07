4b6c32a0d3311ac6ae11f5318eb0edb19ef58dcd
Alan Pearson <pearsonalan@mac.com>
2024-12-07 08:55:24 -0800

    Split into separate files

--- tft-display/ui/tft-ui.cc
+++ tft-display/ui/tft-ui.cc
@@ -2,288 +2,16 @@
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
 #include <Adafruit_FT6206.h> // Touch Screen Library
 
+#include "ui.h"
+
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-#define	BLACK   0x0000
-#define WHITE   0xFFFF
-#define	RED     0xF800
-#define	BLUE    0x001F
-
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 Adafruit_FT6206 ts;
-
-void PrintPoint(const TS_Point& p) {
-  Serial.print("(");
-  Serial.print(p.x);
-  Serial.print(",");
-  Serial.print(p.y);
-  Serial.print(")");
-}
-
-class Event {
-public:
-  virtual ~Event() = default;
-};
-
-enum class TouchEventType {
-  None = 0,
-  Begin = 1,
-  Drag = 2,
-  End = 3,
-};
-
-class TouchEvent : public Event {
-public:
-  TouchEvent(TouchEventType type, const TS_Point& p) : type_(type), point_(p) {}
-  ~TouchEvent() override = default;
-
-  TouchEventType type() const { return type_; }
-  const TS_Point& point() const { return point_; }
-
-protected:
-  TouchEventType type_ = TouchEventType::None;
-  TS_Point point_;
-};
-
-class Window {
-public:
-  Window(int x, int y, int cx, int cy) :
-      x_(x), y_(y), cx_(cx), cy_(cy) {}
-
-  virtual ~Window() = default;
-
-  // Update is used to allow a window to update it's state (e.g. by reading
-  // external inputs.) A window should not draw during Update();
-  virtual void Update() {}
-
-  // Draw is invoked to refresh the Window's display on the screen.
-  virtual void Draw(Adafruit_GFX* gfx) {
-    // Window is valid after drawing.
-    Validate();
-  } 
-
-  // Return true if the touch event was handled, else false.
-  virtual bool onTouch(const TouchEvent& event) = 0;
-
-  virtual bool NeedsRefresh() const {
-    // Default behavior is that we only need refresh if invalid.
-    return !isValid();
-  }
-
-  int x() const { return x_; }
-  int y() const { return y_; }
-  int cx() const { return cx_; }
-  int cy() const { return cy_; }
-
-  Window* parent() const { return parent_; }
-  void setParent(Window* w) { parent_ = w; }
-
-  Window* nextSibling() const { return next_sibling_; }
-  void setNextSibling(Window* w) { next_sibling_ = w; }
-
-  bool isValid() const { return is_valid_; }
-  void Invalidate() { is_valid_ = false; }
-  void Validate() { is_valid_ = true; }
-
-  bool PointInWindow(const TS_Point& p) {
-    return (p.x >= x_ &&
-            p.x <= x_ + cx_ &&
-            p.y >= y_ &&
-            p.y <= y_ + cy_);
-  }
-
-protected:
-  int x_;
-  int y_;
-  int cx_;
-  int cy_;
-
-  Window* parent_ = nullptr;
-  Window* next_sibling_ = nullptr;
-  bool is_valid_ = false;
-};
-
-
-// A Panel is a container window that holds other windows.
-class Panel : public Window {
-public:
-  Panel(int x, int y, int cx, int cy) : Window(x, y, cx, cy) {}
-  ~Panel() override = default;
-
-  void Update() override;
-  void Draw(Adafruit_GFX* gfx) override;
-
-  bool onTouch(const TouchEvent& event) override;
-  bool NeedsRefresh() const override { return true; } 
-
-  void AddChild(Window* w);
-
-protected:
-  Window* first_child_ = nullptr;
-};
-
-void Panel::AddChild(Window* w) {
-  w->setParent(this);
-  if (!first_child_) {
-    first_child_ = w;
-  } else {
-    Window* last_child = first_child_;
-    while (last_child->nextSibling() != nullptr) {
-      last_child = last_child->nextSibling();
-    }
-    last_child->setNextSibling(w);
-  }
-}
-
-bool Panel::onTouch(const TouchEvent& event) {
-  if (!PointInWindow(event.point())) {
-    return false;
-  }
-
-  for (Window* w = first_child_; w != nullptr; w = w->nextSibling()) {
-    if (w->onTouch(event)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-void Panel::Update() {
-  for (Window* w = first_child_; w != nullptr; w = w->nextSibling()) {
-    w->Update();
-  }
-  Window::Update();
-}
-
-void Panel::Draw(Adafruit_GFX* gfx) {
-  for (Window* w = first_child_; w != nullptr; w = w->nextSibling()) {
-    if (w->NeedsRefresh()) {
-      w->Draw(gfx);
-    }
-  }
-  Window::Draw(gfx);
-}
-
-class Button : public Window {
-public:
-  Button(int x, int y, int cx, int cy, const char* text)
-      : Window(x, y, cx, cy), text_(text) {}
-  ~Button() override = default;
-
-  void Draw(Adafruit_GFX* gfx) override;
-  bool onTouch(const TouchEvent& event) override;
-
-  virtual void onPress(const TouchEvent& event) {}
-
-  const String& getText() const { return text_; }
-
-protected:
-  String text_;
-};
-
-void Button::Draw(Adafruit_GFX* gfx) {
-  gfx->fillRect(x(), y(), cx(), cy(), 0xEF5D);
-
-  gfx->drawFastHLine(x(), y(), cx(), 0xF7BE);
-  gfx->drawFastHLine(x(), y() + 1, cx(), 0xF7BE);
-  
-  gfx->drawFastVLine(x(), y(), cy(), 0xF7BE);
-  gfx->drawFastVLine(x() + 1, y(), cy(), 0xF7BE);
-
-  gfx->drawFastHLine(x(), y() + cy() - 1, cx(), 0xBDF7);
-  gfx->drawFastHLine(x(), y() + cy() - 2, cx(), 0xBDF7);
-
-  gfx->drawFastVLine(x() + cx() - 1, y(), cy(), 0xBDF7);
-  gfx->drawFastVLine(x() + cx() - 2, y(), cy(), 0xBDF7);
-
-  int16_t tx, ty;
-  uint16_t bx, by;
-  gfx->setTextColor(WHITE);
-  gfx->setTextSize(1);
-  gfx->getTextBounds(getText(), x(), y(), &tx, &ty, &bx, &by);
-  Serial.print("TEXT BOUNDS: ");
-  Serial.print(bx);
-  Serial.print(",");
-  Serial.println(by);
-  gfx->setCursor(x() + (cx() - bx) / 2, y() + (cy() - by) / 2);
-  gfx->print(getText());
-  Window::Draw(gfx);
-}
-
-bool Button::onTouch(const TouchEvent& event) {
-  if (PointInWindow(event.point())) {
-    if (event.type() == TouchEventType::End) {
-      Serial.print("Button Press: ");
-      Serial.println(getText());
-      onPress(event);
-    }
-    return true;
-  }
-  return false;
-}
-
-class Label : public Window {
-public:
-  Label(int x, int y, int cx, int cy, const String& text,
-        int16_t fg=WHITE, int16_t bg=BLACK, int size=1)
-      : Window(x, y, cx, cy), text_(text), fg_(fg), bg_(bg), size_(size) {}
-  ~Label() override = default;
-
-  void Draw(Adafruit_GFX* gfx) override;
-  bool onTouch(const TouchEvent& event) override { return false; }
-
-  const String& getText() const { return text_; }
-  void setText(const String& text) {
-    text_ = text;
-    Invalidate();
-  }
-
-protected:
-  String text_;
-  int16_t fg_ = WHITE;
-  int16_t bg_ = BLACK;
-  int size_ = 1;
-};
-
-void Label::Draw(Adafruit_GFX* gfx) {
-  Serial.print("Drawing Label@");
-  Serial.println((unsigned long)(void*)this, HEX);
-
-  gfx->fillRect(x(), y(), cx(), cy(), bg_);
-  gfx->setCursor(x(), y());
-  gfx->setTextColor(fg_);
-  gfx->setTextSize(size_);
-  gfx->print(getText());
-  Window::Draw(gfx);
-}
-
-class SmoothLabel : public Label {
-public:
-  SmoothLabel(int x, int y, int cx, int cy, const String& text,
-        int16_t fg=WHITE, int16_t bg=BLACK, int size=1)
-      : Label(x, y, cx, cy, text, fg, bg, size) {}
-  ~SmoothLabel() override = default;
-
-  void Draw(Adafruit_GFX* gfx) override;
-};
-
-void SmoothLabel::Draw(Adafruit_GFX* gfx) {
-  for (int y = 0; y < cy(); y++) {
-    GFXcanvas16 canvas(cx(), 1);
-    canvas.fillScreen(bg_);
-    canvas.setCursor(0, 0 - y);
-    canvas.setTextColor(fg_);
-    canvas.setTextSize(size_);
-    canvas.print(getText());
-    gfx->drawRGBBitmap(x(), this->y() + y, canvas.getBuffer(),
-        canvas.width(), canvas.height());
-  }
-  Window::Draw(gfx);
-}
 
 class TimeLabel : public SmoothLabel {
 public:
@@ -296,6 +24,22 @@ public:
 
 protected:
   char display_time_[16];
+};
+
+class OutputButton : public Button {
+public:
+  OutputButton(int x, int y, int cx, int cy, const char* text, Label* output_label) :
+    Button(x, y, cx, cy, text), output_label_(output_label) {}
+
+  void onPress(const TouchEvent&) override {
+    Serial.println("In OutputButton::onPress");
+    String s = getText();
+    s += " pressed";
+    output_label_->setText(s.c_str());
+  }
+
+protected:
+  Label* const output_label_;
 };
 
 void TimeLabel::Update() {
@@ -340,21 +84,6 @@ void setup() {
   Serial.println(micros() - start);
 }
 
-void DispatchProc(Window* mainWindow) {
-  mainWindow->Update();
-  if (mainWindow->NeedsRefresh()) {
-    mainWindow->Draw(&tft);
-  }
-}
-
-void RotateTouchPoint(TS_Point* p, uint8_t r) {
-  uint16_t x, y;
-  x = 320 - p->y;
-  y = p->x;
-  p->x = x;
-  p->y = y;
-}
-
 int main() {
   init();
 
@@ -376,21 +105,6 @@ int main() {
   panel.AddChild(&output);
   panel.AddChild(&time);
 
-
-  class OutputButton : public Button {
-  public:
-    OutputButton(int x, int y, int cx, int cy, const char* text, Label* output_label) :
-      Button(x, y, cx, cy, text), output_label_(output_label) {}
-    void onPress(const TouchEvent&) override {
-      Serial.println("In OutputButton::onPress");
-      String s = getText();
-      s += " pressed";
-      output_label_->setText(s.c_str());
-    }
-  protected:
-    Label* const output_label_;
-  };
-
   OutputButton button1(3, 3, 80, 40, "Button 1", &output);
   panel.AddChild(&button1);
 
@@ -399,40 +113,5 @@ int main() {
 
   setup();
 
-  bool is_touched = false;
-  TS_Point last(0,0,0);
-
-  for (;;) {
-    TS_Point p(0,0,0);
-
-    if (ts.touched()) {
-      p = ts.getPoint();
-      RotateTouchPoint(&p, 3);
-    }
-
-    if (ts.touched()) {
-      if (is_touched) {
-        if (p != last) {
-          Serial.print("DRAG TO ");
-          PrintPoint(p);
-          Serial.println();
-        }
-      } else {
-        Serial.print("TOUCH AT ");
-        PrintPoint(p);
-        Serial.println();
-      }
-      is_touched = true;
-    } else {
-      if (is_touched) {
-        Serial.println("TOUCH END");
-        TouchEvent event(TouchEventType::End, last);
-        panel.onTouch(event);
-      }
-      is_touched = false;
-    }
-    last = p;
-
-    DispatchProc(&panel);
-  }
+  UILoop(&tft, &ts, &panel);
 }
